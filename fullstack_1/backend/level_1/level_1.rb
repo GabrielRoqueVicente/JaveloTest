7bc89e479bad7a741e57fa50d3167f1eb4d7f1d9
Gabriel Roque Vicente <gabriel.vicente.59@gmail.com>
2018-09-09 17:15:00 +0200

    Backend level_1

---
+++ fullstack_1/backend/level_1/level_1.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'json'
+require 'pp'
+
+json = File.read('data/input.json')
+obj = JSON.parse(json)
+
+objectives = obj["objectives"]
+progressRecords = obj["progress_records"]
+
+
+def progressPercent(target,start,value )
+	progress = value*100/(target-start)
+	return progress
+end
+newProgressRecords = []
+
+progressRecords.each do |record|
+	recordId = record["id"]
+	objectives.each do |objective|
+		if objective["id"] == recordId
+			progress = progressPercent(objective["target"], objective["start"], record["value"])
+			newProgressRecords << {id: recordId, progress: progress} 
+		end		
+	end
+end
+
+newProgressRecords = {progress_records: newProgressRecords}
+JSON.pretty_generate(newProgressRecords)
+
+File.open("output.json","w") do |f|
+  f.write(newProgressRecords.to_json)
+end
+
+pp newProgressRecords
